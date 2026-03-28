dca83c6318e7c5f42403f19ae44c822515e1b3cf
kukimoto nobuyuki <kukimoto@me>
2026-03-28 16:59:03 +0900

    dell me

--- Class1.cs
+++ Class1.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//つかっていない
 namespace XRServer
 {
     internal class Class1
     {
+        //消しても構わない
     }
 }
